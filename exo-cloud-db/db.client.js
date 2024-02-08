b27bf6f5a5aa848f3868b13d7ddbcccfad3f11da
RENARD Adrien <99894313+BananaPancake76@users.noreply.github.com>
2024-02-08 11:15:30 +0100

    Update db.client.js

--- exo-cloud-db/db.client.js
+++ exo-cloud-db/db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://esgi_cloud_exam_db_86954_user:ciAqrIMnHzv0gsyilkVZCLaa0sqXNpSZ@dpg-cn2acrv109ks7394o8r0-a/esgi_cloud_exam_db_86954', // TODO
+  'postgres://esgi_cloud_exam_db_86954_user:ciAqrIMnHzv0gsyilkVZCLaa0sqXNpSZ@dpg-cn2acrv109ks7394o8r0-a.oregon-postgres.render.com/esgi_cloud_exam_db_86954', // TODO
   {
     dialectOptions: {
       ssl: {
